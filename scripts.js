ad550d1b6c4531245cc903075466480326e620e5
Angelica <angelicabbate@gmail.com>
2025-07-07 21:05:09 -0300

    placing information and adjust sections

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 // Cambiar navbar al hacer scroll
-window.addEventListener('scroll', function() {
-  const navbar = document.querySelector('.navbar');
-  navbar.classList.toggle('scrolled', window.scrollY > 50);
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
+});
+
+// Inicializar swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
