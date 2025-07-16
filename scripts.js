7baeb47f90746a033badb2fb62d7f91dc1b35d65
Angelica <angelicabbate@gmail.com>
2025-07-16 18:18:14 -0300

    adjust suscription cards

--- scripts.js
+++ scripts.js
@@ -4,9 +4,6 @@ window.addEventListener("scroll", function () {
   navbar.classList.toggle("scrolled", window.scrollY > 50);
 });
 
-// Inicializar wow
-new WOW().init();
-
 // Inicializar swiper
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: "auto",
@@ -28,29 +25,4 @@ var swiper = new Swiper(".mySwiper", {
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
   // gsap code here!
-});
-
-// script de intro
-
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("bubble-container");
-  const bubbles = container.querySelectorAll(".bubble");
-
-  bubbles.forEach((bubble) => {
-    const size = Math.random() * 40 + 20; // 20 a 60px
-    bubble.style.width = `${size}px`;
-    bubble.style.height = `${size}px`;
-    bubble.style.left = `${Math.random() * 100}%`;
-
-    gsap.to(bubble, {
-      y: -350,
-      duration: 5 + Math.random() * 4,
-      repeat: -1,
-      ease: "sine.inOut",
-      delay: Math.random() * 2,
-      onRepeat: () => {
-        bubble.style.left = `${Math.random() * 100}%`;
-      },
-    });
-  });
 });
