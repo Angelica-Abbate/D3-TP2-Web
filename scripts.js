9dfb7fcdf1627f699e57f76e63413a6f1cedbd97
Angelica <angelicabbate@gmail.com>
2025-07-17 20:32:11 -0300

    add wow animations again, to home page

--- scripts.js
+++ scripts.js
@@ -20,9 +20,7 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+// Inicializar wow
+new WOW().init();
+
 // inicializar GSAP
-// use a script tag or an external JS file
-document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-  // gsap code here!
-});
